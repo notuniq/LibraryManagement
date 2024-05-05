5f19c7868d5dc186d6e8f2d5e984eb50751ad28d
unibtw <ihackonelove@gmail.com>
2024-05-05 17:44:00 +0500

    v1.0.2
    FIxed routers

--- server/routes/bookRoutes.js
+++ server/routes/bookRoutes.js
@@ -7,9 +7,9 @@ const bookController = require('../controllers/bookController');
 // Маршруты для добавления, получения всех книг и удаления книги
 router.post('/books', bookController.addBook); // POST запрос для добавления книги
 router.get('/books', bookController.getAllBooks); // GET запрос для получения всех книг
-router.delete('/books/:id', bookController.deleteBook); // DELETE запрос для удаления книги
+router.delete('/books/info/:id', bookController.deleteBook); // DELETE запрос для удаления книги
 // Маршрут для поиска книги
-router.get('/books/:id', bookController.findBook); // GET запрос для поиска книги по ID
+router.get('/books/info/:id', bookController.findBook); // GET запрос для поиска книги по ID
 // Маршрут для поиска книги по названию и/или автору
 router.get('/books/search', bookController.searchBooks); // GET запрос для поиска книги по названию и/или автору
 
